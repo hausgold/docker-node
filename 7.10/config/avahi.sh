e758ea7496283f64a3930f6f6cc73ffc15ff1661
Hermann Mayer <jack@hermann-mayer.net>
2017-08-07 11:38:53 +0200

    Improved the cleanup on avahi starts.

--- 7.10/config/avahi.sh
+++ 7.10/config/avahi.sh
@@ -29,5 +29,11 @@ sed \
   -e 's/^\(rlimit\)/#\1/g' \
   -i /etc/avahi/avahi-daemon.conf
 
+# If a avahi daemon is running, kill it
+avahi-daemon -c && avahi-daemon -k
+
+# Clean up orphans
+rm -rf /run/avahi-daemon/{pid,socket}
+
 # Start avahi
-/usr/sbin/avahi-daemon --no-rlimits
+avahi-daemon --no-rlimits
